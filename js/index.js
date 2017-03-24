f56595f548db3ce636f62878089f24042e961ad8
Roy33 <1270679840@qq.com>
2017-03-24 22:37:13 +0800

    -a

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2017-03-17 08:13:51
+ * @version $Id$
+ */
+//document.querySelector('.td').addEventListener('click',function(){alert('a');})
+//动态绑定事件
+
+
